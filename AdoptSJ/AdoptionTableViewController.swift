f26b7101e1166ce70921ae342490a4034756dab4
Steven Booth <StevenMac@Stevens-MacBook-Pro.local>
2017-03-04 13:04:44 -0800

    Not quite working

--- AdoptSJ/AdoptionTableViewController.swift
+++ AdoptSJ/AdoptionTableViewController.swift
@@ -30,13 +30,13 @@ class AdoptionTableViewController: UITableViewController {
     override func numberOfSectionsInTableView(tableView: UITableView!) -> Int {
         // #warning Potentially incomplete method implementation.
         // Return the number of sections.
-        return 0
+        return 3
     }
 
     override func tableView(tableView: UITableView!, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete method implementation.
         // Return the number of rows in the section.
-        return 1
+        return 5
     }
 
     
