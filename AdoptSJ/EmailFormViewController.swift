07fd0da841bd5526564a3a1cb5f7283c1e049aee
Shivam Desai <sdesai1@scu.edu>
2017-03-04 23:16:58 -0800

    Added code for the Mail Controller to send email with form info

--- AdoptSJ/EmailFormViewController.swift
+++ AdoptSJ/EmailFormViewController.swift
@@ -7,8 +7,9 @@
 //
 
 import UIKit
+import MessageUI
 
-class EmailFormViewController: UIViewController, UITextFieldDelegate, UITextViewDelegate {
+class EmailFormViewController: UIViewController, UITextFieldDelegate, UITextViewDelegate, MFMailComposeViewControllerDelegate {
 
     var name: String?
     var address: String?
@@ -86,7 +87,31 @@ class EmailFormViewController: UIViewController, UITextFieldDelegate, UITextView
     }
     */
 
+    @IBAction func createEmailToSend(_ sender: UIButton){
+        if !MFMailComposeViewController.canSendMail() {
+            print("Mail services are not available")
+            return
+        }
+        
+        let composeVC = MFMailComposeViewController()
+        composeVC.mailComposeDelegate = self
+        
+        // Configure the fields of the interface.
+        composeVC.setToRecipients(["nsgood2@gmail.com"])
+        composeVC.setSubject("Hello!")
+        composeVC.setMessageBody("Hello from California!", isHTML: false)
+        
+        // Present the view controller modally.
+        self.present(composeVC, animated: true, completion: nil)
+    }
     
+    func mailComposeController(controller: MFMailComposeViewController,
+                               didFinishWithResult result: MFMailComposeResult, error: NSError?) {
+        // Check the result or perform other tasks.
+        
+        // Dismiss the mail compose view controller.
+        controller.dismiss(animated: true, completion: nil)
+    }
     
     
     
